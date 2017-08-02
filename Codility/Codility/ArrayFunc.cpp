62c8606f9a92f853905c25f1c903bd4c8ba1b86d
Elina <elina8721@gmail.com>
2017-08-02 13:00:56 +0800

    Lesson2 - CyclicRotation

---
+++ Codility/Codility/ArrayFunc.cpp
@@ -0,0 +1,18 @@
+#include "ArrayFunc.h"
+
+vector<int> CyclicRotation(vector<int> &A, int K)
+{
+    if (A.empty()) { return A; }
+
+    int i, j;
+    for (i = 0; i < K; i++)
+    {
+        int temp = A.back();
+        for (j = A.size() - 1; j > 0; j--)
+        {
+            A[j] = A[j - 1];
+        }
+        A[j] = temp;
+    }
+    return A;
+}
